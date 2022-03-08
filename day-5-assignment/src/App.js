746dbd192917e9508faa9494d1f2cf668c9549ca
Bhavesh-XLV <95977977+Bhavesh-XLV@users.noreply.github.com>
2022-03-09 01:51:18 +0530

    Add files via upload

---
+++ day-5-assignment/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Todos, { Groceries } from "./components/Grocery";
+
+function App() {
+  return (
+    <div className="App">
+      <Groceries />
+    </div>
+    // <div className="App">
+    //   <Todos />
+    // </div>
+  );
+}
+
+export default App;
